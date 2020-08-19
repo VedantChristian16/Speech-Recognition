3dfb23c5c481a7ff3021e61734eb0eac29d77302
VedantChristian16 <55919296+VedantChristian16@users.noreply.github.com>
2020-08-19 17:26:43 +0100

    Add files via upload

---
+++ Speech Recognition.py
@@ -0,0 +1,23 @@
+'''
+Created by Vedant Christian
+Created on 19 / 08 / 2020
+'''
+
+import speech_recognition as sr
+import webbrowser
+
+sr.Microphone(device_index=(1))
+r = sr.Recognizer()
+r.energy_threshold = 5000
+
+with sr.Microphone() as source:
+    print("Speak!")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said: {}".format(text))
+        url = 'https://www.google.com/search?q='
+        search_url = url + text
+        webbrowser.open(search_url)
+    except:
+        print("Can't Recognise")
